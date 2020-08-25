ef1b827a01c1fc098c5efbb5a307eb574b4d8873
Azamat9904 <azamatnamialy@gmail.com>
2020-08-25 21:14:01 +0600

    Refractoring

--- controller/generalController.js
+++ controller/generalController.js
@@ -1,6 +1,7 @@
 exports.setHeaders = (req, res, next) => {
-    const allowedHost = ['http://localhost:4200'];
-    if(allowedHost.indexOf(origin) !== -1)  res.header('Access-Control-Allow-Origin', origin);
+    // const allowedHost = ['http://localhost:4200'];
+    // if(allowedHost.indexOf(origin) !== -1)  res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods','GET, POST, PUT');
     res.header('Access-Control-Allow-Headers','Content-Type, Authorization, Origin, Accept, X-Requested-With');
     res.header('Access-Control-Allow-Credentials',true);
